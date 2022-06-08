f9b6824135fab3641ec6763c9b62f1bc2a37a25a
June060522 <a80336234@gmail.com>
2022-06-08 11:54:59 +0900

    Scene
    -add StartScene
    -add PlayScene
    -add changeSceneScript

--- MirrorShooter/Assets/02.Scripts/Player/PlayerManager.cs
+++ MirrorShooter/Assets/02.Scripts/Player/PlayerManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -40,6 +41,7 @@ public class PlayerManager : MonoBehaviour
         {
             Destroy(gameObject);
             Destroy(WhitePlayer);
+            SceneManager.LoadScene("StartScene");
         }
     }
 
